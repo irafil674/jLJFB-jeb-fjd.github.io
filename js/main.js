227044e5df7960d89a4933c76b46ed2c3e5fa0ee
irafil674 <73308566+irafil674@users.noreply.github.com>
2025-04-29 20:53:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,3 +22,14 @@ function MyFunc(name){
     }
     return myFunction;
 }
+
+tg.expand(); //расширяем на все окно  
+
+tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
+tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+	tg.sendData("some string that we need to send"); 
+	//при клике на основную кнопку отправляем данные в строковом виде
+});
