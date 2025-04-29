8d88ccdaadbc9aba6b85a9f397dbae166b0e62c0
irafil674 <73308566+irafil674@users.noreply.github.com>
2025-04-29 21:27:17 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 'use strict'
 
-// window.onkeydown = jump_event;
 
 
 for (var i = 1; i < 11; i++) {
     document.getElementById("button" + i).onclick = MyFunc("button" + i);
     };
 
-// button1.onclick = MyFunc("button1"); 
+
+let sb_bt = document.getElementById("sb_bt")
+sb_bt.onclick = myFunction
+
+function myFunction(){
+    tg.sendData("some string that we need to send хы-хы, ха-ха"); 
+}
 
 let tg = window.Telegram.WebApp;
 
@@ -18,18 +23,6 @@ function MyFunc(name){
         else{
             document.getElementById(name).style.backgroundColor='#bde0ff';
         }
-        tg.sendData("some string that we need to send"); 
     }
     return myFunction;
 }
-
-tg.expand(); //расширяем на все окно  
-
-tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
-tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
-tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
-
-Telegram.WebApp.onEvent('mainButtonClicked', function(){
-	tg.sendData("some string that we need to send"); 
-	//при клике на основную кнопку отправляем данные в строковом виде
-});
